433a2e255336292d7ba506aa0be381eb26a3ecab
Samarth Upare <samarth@dieture.com>
2026-03-13 20:15:45 +0530

    overtime changes

---
+++ redtra_customisation/rpayroll/doctype/salary_slip/salary_slip_overtime.py
@@ -0,0 +1,76 @@
+# Copyright (c) 2026, Redtra Customisation and contributors
+# For license information, please see license.txt
+
+import frappe
+from frappe.query_builder.functions import Count
+from frappe.utils import flt
+
+from hrms.payroll.doctype.salary_slip.salary_slip import SalarySlip
+from redtra_customisation.rpayroll.overtime_helpers import (
+	compute_food_allowance_counts,
+	get_overtime_requests_for_period,
+	submit_draft_food_allowance_for_employee_period,
+)
+
+
+class SalarySlipOvertime(SalarySlip):
+	def get_emp_and_working_day_details(self):
+		if self.employee and self.start_date and self.end_date:
+			submit_draft_food_allowance_for_employee_period(self.employee, self.start_date, self.end_date)
+		super().get_emp_and_working_day_details()
+		self.set_overtime_details()
+
+	def on_trash(self):
+		super().on_trash()
+		_update_payroll_entry_if_no_slips_left(self)
+
+	def validate(self):
+		super().validate()
+		if self.employee and self.start_date and self.end_date:
+			self.set_overtime_details()
+
+	def set_overtime_details(self):
+		if not hasattr(self, "overtime_requests"):
+			return
+		self.set("overtime_requests", [])
+		self.total_overtime_hours = 0
+		self.holidays_overtime_hours = 0
+		self.food_allowance_counts = 0
+		start_date = getattr(self, "actual_start_date", None) or self.start_date
+		end_date = getattr(self, "actual_end_date", None) or self.end_date
+		overtime_list = get_overtime_requests_for_period(self.employee, start_date, end_date, only_earned=False)
+		if not overtime_list:
+			return
+		for ot in overtime_list:
+			self.append("overtime_requests", {
+				"overtime_request": ot.name,
+				"overtime_hours": ot.overtime_hours,
+				"is_holiday": ot.is_holiday,
+			})
+			self.total_overtime_hours = flt(self.total_overtime_hours) + flt(ot.overtime_hours)
+			if ot.is_holiday:
+				self.holidays_overtime_hours = flt(self.holidays_overtime_hours) + flt(ot.overtime_hours)
+		payroll_settings = frappe.get_cached_value(
+			"Payroll Settings", None,
+			("enable_food_allowance", "food_allowance_normal_days_threshold", "food_allowance_holiday_weekend_threshold"),
+			as_dict=1,
+		)
+		if payroll_settings and payroll_settings.enable_food_allowance:
+			self.food_allowance_counts = compute_food_allowance_counts(
+				self.employee, start_date, end_date, overtime_list,
+				normal_threshold=flt(payroll_settings.food_allowance_normal_days_threshold) or 12,
+				holiday_threshold=flt(payroll_settings.food_allowance_holiday_weekend_threshold) or 0,
+			)
+
+
+def _update_payroll_entry_if_no_slips_left(doc):
+	if not doc.get("payroll_entry"):
+		return
+	SalarySlip = frappe.qb.DocType("Salary Slip")
+	remaining = frappe.qb.from_(SalarySlip).select(Count(SalarySlip.name)).where(
+		(SalarySlip.payroll_entry == doc.payroll_entry)
+		& (SalarySlip.docstatus != 2)
+		& (SalarySlip.name != doc.name)
+	).run()[0][0]
+	if remaining == 0:
+		frappe.db.set_value("Payroll Entry", doc.payroll_entry, "salary_slips_created", 0)
---
+++ redtra_customisation/setup.py
@@ -0,0 +1,119 @@
+# Copyright (c) 2026, Redtra Customisation and contributors
+# For license information, please see license.txt
+
+
+def get_overtime_custom_fields():
+	return {
+		"Attendance": [
+			{
+				"fieldname": "overtime_section",
+				"fieldtype": "Section Break",
+				"label": "Overtime",
+				"insert_after": "early_exit",
+			},
+			{
+				"fieldname": "overtime_hours",
+				"fieldtype": "Float",
+				"label": "Overtime Hours",
+				"precision": 2,
+				"read_only": 1,
+				"insert_after": "overtime_section",
+			},
+			{
+				"fieldname": "overtime_request",
+				"fieldtype": "Link",
+				"label": "Overtime Request",
+				"options": "Overtime Request",
+				"read_only": 1,
+				"insert_after": "overtime_hours",
+			},
+		],
+		"Payroll Settings": [
+			{
+				"fieldname": "overtime_section",
+				"fieldtype": "Section Break",
+				"label": "Overtime",
+				"insert_after": "column_break_zi9y",
+			},
+			{
+				"default": "0",
+				"fieldname": "auto_create_overtime_request",
+				"fieldtype": "Check",
+				"label": "Auto Create Overtime Request",
+				"description": "Automatically create Overtime Request from Attendance when overtime hours exist",
+				"insert_after": "overtime_section",
+			},
+			{
+				"fieldname": "food_allowance_section",
+				"fieldtype": "Section Break",
+				"label": "Food Allowance",
+				"insert_after": "auto_create_overtime_request",
+			},
+			{
+				"default": "0",
+				"fieldname": "enable_food_allowance",
+				"fieldtype": "Check",
+				"label": "Enable Food Allowance",
+				"insert_after": "food_allowance_section",
+			},
+			{
+				"default": "12",
+				"fieldname": "food_allowance_normal_days_threshold",
+				"fieldtype": "Float",
+				"label": "Normal Days Threshold (Hours)",
+				"insert_after": "enable_food_allowance",
+			},
+			{
+				"default": "0",
+				"fieldname": "food_allowance_holiday_weekend_threshold",
+				"fieldtype": "Float",
+				"label": "Holiday/Weekend Threshold (Hours)",
+				"insert_after": "food_allowance_normal_days_threshold",
+			},
+			{
+				"depends_on": "eval:doc.enable_food_allowance",
+				"fieldname": "food_allowance_salary_component",
+				"fieldtype": "Link",
+				"label": "Food Allowance Salary Component",
+				"options": "Salary Component",
+				"insert_after": "food_allowance_holiday_weekend_threshold",
+			},
+			{
+				"depends_on": "eval:doc.enable_food_allowance",
+				"fieldname": "food_allowance_amount",
+				"fieldtype": "Currency",
+				"label": "Food Allowance Amount (Per Day)",
+				"description": "Amount per qualifying day when threshold is crossed",
+				"insert_after": "food_allowance_salary_component",
+			},
+		],
+		"Salary Slip": [
+			{
+				"fieldname": "overtime_section",
+				"fieldtype": "Section Break",
+				"label": "Overtime",
+				"insert_after": "payment_days",
+			},
+			{
+				"fieldname": "overtime_requests",
+				"fieldtype": "Table",
+				"options": "Salary Slip Overtime Request",
+				"read_only": 1,
+				"insert_after": "overtime_section",
+			},
+			{
+				"fieldname": "holidays_overtime_hours",
+				"fieldtype": "Float",
+				"label": "Holidays Overtime Hours",
+				"read_only": 1,
+				"insert_after": "overtime_requests",
+			},
+			{
+				"fieldname": "food_allowance_counts",
+				"fieldtype": "Int",
+				"label": "Food Allowance Counts",
+				"read_only": 1,
+				"insert_after": "holidays_overtime_hours",
+			},
+		],
+	}
