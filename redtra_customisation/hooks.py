ded1551b44ecfcdfa55289211eb1ab70dfecdcdc
Samarth Upare <samarth@dieture.com>
2026-08-17 01:38:45 +0530

    feat: add erpnext and construction_management as required apps in hooks

--- redtra_customisation/hooks.py
+++ redtra_customisation/hooks.py
@@ -8,7 +8,8 @@ app_license = "mit"
 # Apps
 # ------------------
 
-# required_apps = []
+required_apps = ["erpnext", "construction_management"]
+
 
 # Each item in the list will be shown as an app in the apps page
 # add_to_apps_screen = [
