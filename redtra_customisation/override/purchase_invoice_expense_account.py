aedfaf3bcfb15f9d2ca44f3fd513b7f829ab98ed
Samarth Upare <samarth@dieture.com>
2026-08-17 00:48:36 +0530

    fix: add field existence check for Redtra Custom Setting to prevent attribute errors

--- redtra_customisation/override/purchase_invoice_expense_account.py
+++ redtra_customisation/override/purchase_invoice_expense_account.py
@@ -5,9 +5,12 @@ import frappe
 
 @frappe.whitelist()
 def is_any_account_allowed_on_pi() -> int:
+	if not frappe.get_meta("Redtra Custom Setting").has_field("allow_any_account_on_purchase_invoice"):
+		return 0
 	return int(
 		frappe.db.get_single_value(
 			"Redtra Custom Setting", "allow_any_account_on_purchase_invoice"
 		)
 		or 0
 	)
+
